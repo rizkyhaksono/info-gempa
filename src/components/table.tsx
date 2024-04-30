453e7b187861bef9854e04ddead0f78adc458dec
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-04-30 22:55:10 +0700

    wip (layout): backdrop layout

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -2,31 +2,33 @@ import type Gempa from "../types/gempa"
 
 export default function Table({ data }: { data: any }) {
   return (
-    <table className="table-auto">
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Jam</th>
-          <th>Lintang</th>
-          <th>Bujur</th>
-          <th>Kedalaman</th>
-          <th>Magnitudo</th>
-          <th>Wilayah</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.Infogempa.gempa.map((gempa: Gempa, index: number) => (
+    <div className="bg-white bg-opacity-[40%] backdrop-blur-lg backdrop-filter rounded-2xl border border-gray-100 p-4">
+      <table className="table-auto">
+        <thead>
           <tr>
-            <td>{index + 1}</td>
-            <td>{gempa.Jam}</td>
-            <td>{gempa.Lintang}</td>
-            <td>{gempa.Bujur}</td>
-            <td>{gempa.Kedalaman}</td>
-            <td>{gempa.Magnitude}</td>
-            <td>{gempa.Wilayah}</td>
+            <th>No</th>
+            <th>Jam</th>
+            <th>Lintang</th>
+            <th>Bujur</th>
+            <th>Kedalaman</th>
+            <th>Magnitudo</th>
+            <th>Wilayah</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.Infogempa.gempa.map((gempa: Gempa, index: number) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{gempa.Jam}</td>
+              <td>{gempa.Lintang}</td>
+              <td>{gempa.Bujur}</td>
+              <td>{gempa.Kedalaman}</td>
+              <td>{gempa.Magnitude}</td>
+              <td>{gempa.Wilayah}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
