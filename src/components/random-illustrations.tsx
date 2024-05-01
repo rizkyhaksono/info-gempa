5849ed062ecfb815ac183f27a385970de37346e9
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-05-01 09:40:32 +0700

    feat (random-image): rename file

--- src/components/random-illustrations.tsx
+++
@@ -1,7 +0,0 @@
-export default function RandomImage() {
-  return (
-    <div className="absolute -z-10 w-full">
-      <img src={`https://source.unsplash.com/random/1800x1800/?bali`} alt="Random" className="w-full h-full object-cover object-center" loading="lazy" />
-    </div>
-  )
-}
---
+++ src/components/random-image.tsx
@@ -0,0 +1,7 @@
+export default function RandomImage() {
+  return (
+    <div className="absolute -z-10 w-full">
+      <img src={`https://source.unsplash.com/random/1800x1800/?bali`} alt="Random" className="w-full h-auto md:h-full object-cover object-center" loading="lazy" />
+    </div>
+  )
+}
