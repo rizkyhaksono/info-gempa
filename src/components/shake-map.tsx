a5bcf859ffea1f3ba5a9ff6fed87f63295abcb61
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-06-05 20:47:05 +0700

    fix(map): month parser

--- src/components/shake-map.tsx
+++ src/components/shake-map.tsx
@@ -1,6 +1,6 @@
 export default function ShakeMap({ data }: Readonly<{ data: any }>) {
-  const originalTanggal = data?.Infogempa?.gempa[0].Tanggal
-  const originalJam = data?.Infogempa?.gempa[0].Jam
+  const originalTanggal = data?.Infogempa?.gempa[0].Tanggal;
+  const originalJam = data?.Infogempa?.gempa[0].Jam;
 
   function formatDate(inputDate: string) {
     const monthNames = {
@@ -9,40 +9,43 @@ export default function ShakeMap({ data }: Readonly<{ data: any }>) {
       Maret: "03",
       April: "04",
       Mei: "05",
-      Juni: "06",
-      Juli: "07",
+      Jun: "06",
+      Jul: "07",
       Agustus: "08",
       September: "09",
       Oktober: "10",
       November: "11",
       Desember: "12",
-    }
+    };
 
-    const parts = inputDate.split(" ")
-    const day = parts[0]
-    const monthName = parts[1]
-    const year = parts[2]
+    const parts = inputDate.split(" ");
+    const day = parts[0];
+    const monthName = parts[1];
+    const year = parts[2];
+    console.log(day);
+    console.log(monthName);
+    console.log(year);
 
-    const monthNumber = monthNames[monthName as keyof typeof monthNames]
-    const formattedDate = `${year}${monthNumber}${day.padStart(2, "0")}`
-    return formattedDate
+    const monthNumber = monthNames[monthName as keyof typeof monthNames];
+    const formattedDate = `${year}${monthNumber}${day.padStart(2, "0")}`;
+    return formattedDate;
   }
 
   function formatTime(inputTime: string) {
-    const [time] = inputTime.split(" ")
-    const [hour, minute, second] = time.split(":")
+    const [time] = inputTime.split(" ");
+    const [hour, minute, second] = time.split(":");
 
-    const formattedTime = `${hour}${minute}${second}`
-    return formattedTime
+    const formattedTime = `${hour}${minute}${second}`;
+    return formattedTime;
   }
 
-  const formattedDate = formatDate(originalTanggal)
-  const formattedTime = formatTime(originalJam)
-  const shakeTime = `${formattedDate}${formattedTime}`
+  const formattedDate = formatDate(originalTanggal);
+  const formattedTime = formatTime(originalJam);
+  const shakeTime = `${formattedDate}${formattedTime}`;
 
   return (
     <div className="mt-5">
       <img src={`https://data.bmkg.go.id/DataMKG/TEWS/${shakeTime}.mmi.jpg`} alt="Shake Map" loading="lazy" className="rounded-lg w-full" />
     </div>
-  )
+  );
 }
